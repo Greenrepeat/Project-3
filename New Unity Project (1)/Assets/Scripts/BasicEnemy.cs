b1f424c06a95f50fd2f4956783a57b0c2aa0922d
TheWildPainter <jd051201@gmail.com>
2020-04-16 20:46:47 +0800

    FIXED

---
+++ New Unity Project (1)/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public Transform target;
+    private float speed = 3f;
+    private float attack1Range = 1f;
+    public int attack1Damage = 1;
+    public float timeBetweenAttacks;
+
+    public void SettimeBetweenAttacks(float value)
+    {
+        SettimeBetweenAttacks(value);
+    }
+
+    void Start()
+    {
+        Rest();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void MoveToPlayer()
+    {
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+        if (Vector3.Distance(transform.position, target.position) > attack1Range)
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+
+    public void Rest()
+    {
+
+    }
+}
---
+++ New Unity Project (1)/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public BoxCollider territory;
+    GameObject player;
+    bool playerInTerritory;
+
+    public GameObject enemy;
+    BasicEnemy basicenemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInTerritory == true)
+        {
+            basicenemy.MoveToPlayer();
+        }
+        if (playerInTerritory == false)
+        {
+            basicenemy.Rest();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInTerritory = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInTerritory = false;
+        }
+    }
+}
