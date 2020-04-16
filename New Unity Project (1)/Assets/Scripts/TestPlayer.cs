ae2d82c55d1273841fc4b5d6c84fb3eeb6f64bbc
Greenrepeat <61007678+Greenrepeat@users.noreply.github.com>
2020-04-16 16:20:49 +0800

    Health Bar, Respawn, Test Map, Test Player

---
+++ New Unity Project (1)/Assets/Scripts/TestPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+
+    public int maxHealth = 20;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           TakeDamage(5);
+        }
+        
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+
+
+}
+
