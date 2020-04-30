6b0783dc01e98a661fee143509ab8f807f13fc15
Greenrepeat <61007678+Greenrepeat@users.noreply.github.com>
2020-04-30 15:53:35 +0800

    actual game, failed button(?)

---
+++ New Unity Project (1)/Assets/Scripts/Harp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Harp : MonoBehaviour
+{
+
+
+    // changing the damage and range the harp can use with float statements
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fire1 in unity is coded to be left mouse button
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+
+
+    }
+
+    void shoot()
+    {
+        // detects if something was hit by the invisible ray
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+}
